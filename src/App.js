c2d7cd97e071e178a1bcceca97cd1a96181b2370
guille garcia <ggarciacardiel@gmail.com>
2023-03-10 18:25:24 +0100

    adds loader

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import Navbar from './components/Navbar';
 import ErrorPage from './views/ErrorPage';
 import { Routes, Route } from 'react-router-dom'
 import axios from 'axios'
+import {BallTriangle} from 'react-loader-spinner';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios('https://ih-countries-api.herokuapp.com/countries');
       setCountries(result.data);
+      setIsLoading(false);
     };
     fetchData();
   }, []);
@@ -21,21 +24,34 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route 
-          path="/" 
-          element={
-            <CountriesList 
-              countries={countries}/>
+      {isLoading ?
+        <div className="loader-container">
+          <BallTriangle
+            type="Puff"
+            color="#00BFFF"
+            height={100}
+            width={100}
+            timeout={3000} 
+          />
+          <p className="loading-text">Fetching countries...</p>
+        </div>
+        :
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <CountriesList 
+                countries={countries}/>
+              }/>
+          <Route 
+            path="/countries/:countryId" 
+            element={
+              <CountryDetails 
+                countries={countries}/>
             }/>
-        <Route 
-          path="/countries/:countryId" 
-          element={
-            <CountryDetails 
-              countries={countries}/>
-          }/>
-        <Route path="*" element={<ErrorPage />}/>  
-      </Routes>
+          <Route path="*" element={<ErrorPage />}/>  
+        </Routes>
+      }
     </div>
   );
 }
