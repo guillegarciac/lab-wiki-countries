99c8bca32179cc0f4d887860e242437c803c76e5
guille garcia <ggarciacardiel@gmail.com>
2023-03-10 19:35:36 +0100

    updates Navbar to have backk navigation via useNavigate and useLocation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { NavLink } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Navbar() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const showBackButton = location.pathname !== "/";
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-      <NavLink className="navbar-brand" style={{marginLeft: "25px"}} to="/">LAB - Wiki Countries</NavLink>
+      {showBackButton && (
+        <button className="btn-back navbar-brand" onClick={() => navigate(-1)}>
+          &lt; Back
+        </button>
+      )}
+      {!showBackButton && (
+        <NavLink className="navbar-brand mx-auto" to="/" style={{ width: '300px' }}>
+          LAB - Wiki Countries
+        </NavLink>
+      )}
     </nav>
   );
 }
