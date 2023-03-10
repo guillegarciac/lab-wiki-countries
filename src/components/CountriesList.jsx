652165f87222edc18eb130dbaed40f247450a401
guille garcia <ggarciacardiel@gmail.com>
2023-03-10 17:23:58 +0100

    adds Search to CountryList

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function CountriesList({ countries }) {
-  const sortedCountries = countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
-  
+  const [searchValue, setSearchValue] = useState('');
+
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchValue.toLowerCase())
+  );
+
   return (
-    <div className="list-group">
-      {sortedCountries.map(country => 
-        <div className="list-group-item" key={country.alpha3Code}>
-          <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={`${country.name.common}`}/>
-          <Link to={`countries/${country.alpha3Code}`}>{country.name.common}</Link>
-        </div>)}
-    </div>    
-  )
+    <div>
+      <div className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search for a country..."
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+        />
+      </div>
+      <div className="list-group">
+        {filteredCountries.map((country) => (
+          <div className="list-group-item" key={country.alpha3Code}>
+            <img
+              src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+              alt={`${country.name.common}`}
+            />
+            <Link to={`countries/${country.alpha3Code}`}>{country.name.common}</Link>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
