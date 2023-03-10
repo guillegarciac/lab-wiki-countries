a5852f3a9d5d38e2a525e71c3ec075f44a36ec64
guille garcia <ggarciacardiel@gmail.com>
2023-03-10 17:20:40 +0100

    adds sort AtoZ

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function CountriesList({ countries }) {
+  const sortedCountries = countries.sort((a, b) => a.name.common.localeCompare(b.name.common));
+  
   return (
     <div className="list-group">
-      {countries.map(country => 
+      {sortedCountries.map(country => 
         <div className="list-group-item" key={country.alpha3Code}>
           <img src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`} alt={`${country.name.common}`}/>
           <Link to={`countries/${country.alpha3Code}`}>{country.name.common}</Link>
