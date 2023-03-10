3e65f2ea840447ce24e4111b8ddaa88e075a8ed7
guille garcia <ggarciacardiel@gmail.com>
2023-03-10 17:18:41 +0100

    it4 - Fetch countries data from an API (axios)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import countriesData from './countries.json'
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 import Navbar from './components/Navbar';
 import { Routes, Route } from 'react-router-dom'
+import axios from 'axios'
 
 function App() {
-  const [countries, setCountries] = useState(countriesData);
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://ih-countries-api.herokuapp.com/countries');
+      setCountries(result.data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
