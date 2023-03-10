0ba3e9eefc0c2c0187d3734144aa865a1ff8855d
guille garcia <ggarciacardiel@gmail.com>
2023-03-10 17:42:33 +0100

    adds search styles and clearSearch

--- src/components/CountriesList.jsx
+++ src/components/CountriesList.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { AiOutlineSearch, AiOutlineCloseCircle } from 'react-icons/ai';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './CountriesList.css';
 
 export default function CountriesList({ countries }) {
   const [searchValue, setSearchValue] = useState('');
@@ -9,9 +11,16 @@ export default function CountriesList({ countries }) {
     country.name.common.toLowerCase().includes(searchValue.toLowerCase())
   );
 
+  const clearSearch = () => {
+    setSearchValue('');
+  };
+
   return (
     <div>
       <div className="input-group mb-3">
+        <span className="input-group-text">
+          <AiOutlineSearch />
+        </span>
         <input
           type="text"
           className="form-control"
@@ -19,15 +28,22 @@ export default function CountriesList({ countries }) {
           value={searchValue}
           onChange={(e) => setSearchValue(e.target.value)}
         />
+        {searchValue && (
+          <span className="input-group-text" onClick={clearSearch}>
+            <AiOutlineCloseCircle />
+          </span>
+        )}
       </div>
       <div className="list-group">
         {filteredCountries.map((country) => (
           <div className="list-group-item" key={country.alpha3Code}>
-            <img
-              src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
-              alt={`${country.name.common}`}
-            />
-            <Link to={`countries/${country.alpha3Code}`}>{country.name.common}</Link>
+            <div className="country-item">
+              <img
+                src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+                alt={`${country.name.common}`}
+              />
+              <Link to={`countries/${country.alpha3Code}`}>{country.name.common}</Link>
+            </div>
           </div>
         ))}
       </div>
